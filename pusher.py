a7a531351c45904e0a034b6f8933098ae8763dfa
agentswagt <shagatoc@gmail.com>
2021-08-04 16:02:07 +0600

    final_edit

--- pusher.py
+++ pusher.py
@@ -1,9 +1,14 @@
 import os
 
 commit_name = input("Commit_Name: ")
+branch_name = input("Branch Name: ")
 if commit_name == "":
     commit_name = "ignored"
+    print("[+] Commit Name --> ignored")
+if branch_name == "":
+    branch_name = "main"
+    print("[+] Branch Name: Main")
 
 os.system("git add .")
 os.system("git commit -m \"{}\"".format(commit_name))
-os.system("git push -u origin main")
+os.system("git push -u origin \"{}\"".format(branch_name))
