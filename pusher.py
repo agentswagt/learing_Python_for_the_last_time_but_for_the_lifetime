82e9da192ed55a0142f4631609edada0dfadac98
agentswagt <shagatoc@gmail.com>
2021-08-04 16:21:10 +0600

    ignored

--- pusher.py
+++ pusher.py
@@ -5,9 +5,13 @@ branch_name = input("Branch Name: ")
 if commit_name == "":
     commit_name = "ignored"
     print("[+] Commit Name --> ignored")
+else:
+    print("[+] Commit Name --> {}".format(commit_name))
 if branch_name == "":
     branch_name = "main"
     print("[+] Branch Name: Main")
+else:
+    print("[+] Branch Name: {}".format(branch_name))
 
 os.system("git add .")
 os.system("git commit -m \"{}\"".format(commit_name))
