2e8ac1ce017bc6673fdfea992c80e35b487652e6
agentswagt <shagatoc@gmail.com>
2021-08-04 15:49:35 +0600

    ignored

---
+++ pusher.py
@@ -0,0 +1,9 @@
+import os
+
+commit_name = input("Commit_Name: ")
+if commit_name == "":
+    commit_name = "ignored"
+
+os.system("git add .")
+os.system("git commit -m \"{}\"".format(commit_name))
+os.system("git push -u origin master")
